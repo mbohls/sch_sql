0117176944d0e473892dcdc90290be5df76c20df
jenkins <jenkins@ups.com>
2020-04-30 16:16:18 -0500

    first commit

---
+++ mfg/ddl/create_table_02.sql
@@ -0,0 +1,6 @@
+create table table_02 (
+  column_number  number,
+  column_varchar1  varchar2(10),
+  column_varchar2  varchar2(10),
+  column_date  date
+);
---
+++ sales/ddl/sales_01.sql
@@ -0,0 +1,6 @@
+create table sales_01 (
+  column_number  number,
+  column_varchar1  varchar2(10),
+  column_varchar2  varchar2(10),
+  column_date  date
+);
